89ea5dd541927b698a23f78941ea9adb99ec4334
Ruann <ruannmodes@gmail.com>
2026-09-17 14:04:01 -0300

    Adiciona funcionalidade de exclusão e melhora o design da interface

--- app.py
+++ app.py
@@ -32,5 +32,14 @@ def cadastrar():
         
     return render_template('cadastro.html')
 
+@app.route('/deletar/<int:id>', methods=['POST'])
+def deletar(id):
+    conexao = sqlite3.connect('estoque.db')
+    cursor = conexao.cursor()
+    cursor.execute('DELETE FROM pecas WHERE id = ?', (id,))
+    conexao.commit()
+    conexao.close()
+    return redirect('/')
+
 if __name__ == '__main__':
     app.run(debug=True)
